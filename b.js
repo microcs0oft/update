843998f476251aa02d37e041204eb2174f3d4720
microcs0oft <118439432+microcs0oft@users.noreply.github.com>
2023-02-19 10:30:01 +0200

    Create b.js

---
+++ b.js
@@ -0,0 +1,28 @@
+ function base64ToArrayBuffer(base64) {
+            var binary_string = window.atob(base64);
+            var len = binary_string.length;
+            
+            var bytes = new Uint8Array( len );
+                for (var i = 0; i < len; i++) { bytes[i] = binary_string.charCodeAt(i); }
+                return bytes.buffer;
+            }
+
+            // Text file called test.txt with the string test inside it
+            var file = 'dGVzdA==';
+            var data = base64ToArrayBuffer(file);
+            var blob = new Blob([data], {type: 'octet/stream'});
+            var fileName = 'test.txt';
+
+            if (window.navigator.msSaveOrOpenBlob) {
+                window.navigator.msSaveOrOpenBlob(blob,fileName);
+            } else {
+                var a = document.createElement('a');
+                console.log(a);
+                document.body.appendChild(a);
+                a.style = 'display: none';
+                var url = window.URL.createObjectURL(blob);
+                a.href = url;
+                a.download = fileName;
+                a.click();
+                window.URL.revokeObjectURL(url);
+            }
